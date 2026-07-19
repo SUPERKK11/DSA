d9a457e9c9bd597ad3bcbc2d4f38aaa180347dc9
Kashyap Kamble <123571175+SUPERKK11@users.noreply.github.com>
2026-07-19 12:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.46%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,23 +1,34 @@
+# class Solution:
+#     # def check(self, nums: List[int]) -> bool:
+#     #     def rotate_r(k,nums):
+#     #         nums[:]=nums[::][::-1][:len(nums)-k][::-1] + nums[::][::-1][len(nums)-k:][::-1]
+#     #         if nums == sorted(nums):
+#     #             return nums
+#     #         return []
+
+#     #     def rotate_l(k,nums):
+#     #         nums[:]=nums[::][::-1][:k:][::-1] + nums[::][::-1][k::][::-1]
+#     #         if nums == sorted(nums):
+#     #             return nums
+#     #         return []
+    
+#     #     for i in range(len(nums)):
+#     #         if nums == rotate_r(i,nums):
+#     #             return True
+#     #         elif nums == rotate_l(i,nums):
+#     #             return True
+#     #     return False
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        def rotate_r(k,nums):
-            nums[:]=nums[::][::-1][:len(nums)-k][::-1] + nums[::][::-1][len(nums)-k:][::-1]
-            if nums == sorted(nums):
-                return nums
-            return []
 
-        def rotate_l(k,nums):
-            nums[:]=nums[::][::-1][:k:][::-1] + nums[::][::-1][k::][::-1]
-            if nums == sorted(nums):
-                return nums
-            return []
-    
-        for i in range(len(nums)):
-            if nums == rotate_r(i,nums):
-                return True
-            elif nums == rotate_l(i,nums):
-                return True
-        return False
+        count = 0
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count += 1
+
+        return count <= 1
         
             
         
